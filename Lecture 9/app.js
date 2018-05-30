486062b5a6cc93fd5e3e686809b84b7037548cb4
Jason Groff <jgroff88@gmail.com>
2018-05-30 11:49:38 -0500

    finished exercise 9

--- Lecture 9/app.js
+++ Lecture 9/app.js
@@ -1,3 +1,23 @@
-var a = "Hello World!";
+var person = new Object();
 
-function b() {}
+person["firstname"] = "Jason";
+person["lastname"] = "Groff";
+
+var firstNameProperty = "firstname";
+
+console.log(person);
+// console.log(person[firstNameProperty]);
+
+console.log(person.firstname);
+console.log(person.lastname);
+
+person.address = new Object();
+person.address.street = "123 Sunny Lane";
+person.address.city = "Kansas City";
+person.address.state = "Missouri";
+person.address.zip = "64029";
+
+console.log(person.address.street);
+console.log(person.address.city);
+console.log(person["address"]["state"]);
+console.log(person["address"]["zip"]);
